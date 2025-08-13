2bab1768f18c5d2142d9abb23446c762ccdfb89e
Christian Bouwens <christian.bouwens@genomicsengland.co.uk>
2025-08-13 18:08:31 +0100

    filled in temp labkey_loader

--- src/gelpack/datasources/labkey_loader.py
+++ src/gelpack/datasources/labkey_loader.py
@@ -1,1 +1,17 @@
 ## build on the base loader to implement the labkey API
+
+from .base import BaseLoader
+
+
+class LabKeyLoader(BaseLoader):
+    """
+    LabKey-backed loader.
+
+    Uses existing gelpack.gel_utils.lab_to_df under the hood.
+    """
+
+    def _execute(self, spec, **kwargs):
+        from gelpack.gel_utils import lab_to_df
+        return lab_to_df(sql_query=spec, dr=self.version)
+    # for now just grabbing the lab_to_df function for backwards compatibility
+    # but we should implememnt the lab_to_df function within this _execute later.
